72d69c436caa6b19981f63763c7fd2daf2b3ae02
MintarasGrinius <74651345+MintarasGrinius@users.noreply.github.com>
2025-03-30 20:04:27 +0300

    Add theme toggle functionality and update theme context: implement ModeToggle component, enhance ThemeProvider, and adjust Tailwind CSS configuration

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   Routes,
 } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
 import LoginPage from "./pages/login";
 import ServersPage from "./pages/servers";
 
@@ -14,17 +15,19 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <AuthProvider>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/servers" element={<ServersPage />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </Routes>
-        </AuthProvider>
-      </Router>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/servers" element={<ServersPage />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        destructive: "var(--bg-destructive)", // Use the variable
+      },
+    },
   },
   plugins: [],
 };
